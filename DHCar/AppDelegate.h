8e1569aa53454554ea087f8473d2c0f0b9d39e8b
WM <1071597946@qq.com>
2016-05-30 16:57:33 +0800

    Initial Commit

---
+++ DHCar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DHCar
+//
+//  Created by David on 16/5/30.
+//  Copyright © 2016年 WM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
